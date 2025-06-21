6b2fce0dce8d3ff548067a433fd9cf117b479bc4
DevAbdallahSi <abdallah.falt@gmail.com>
2025-06-22 01:16:25 +0300

    deploy

--- TimeTrek/src/main/java/com/TimeTrek/TimeTrekApplication.java
+++ TimeTrek/src/main/java/com/TimeTrek/TimeTrekApplication.java
@@ -1,9 +1,14 @@
 package com.TimeTrek;
 
 import org.apache.catalina.connector.Connector;
+
 import org.apache.coyote.ajp.AbstractAjpProtocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.ajp.AbstractAjpProtocol;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 
